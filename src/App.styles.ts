275383eab9dc59497a87b0e845a2a8bb16206ad7
Maria Eduarda <dudinha.coffacci@hotmail.com>
2023-06-30 10:32:29 -0300

    first commit

---
+++ src/App.styles.ts
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  background-color: #17181f;
+  min-width: 100vw;
+  color: #797a81;
+  min-height: 100vh;
+`;
+
+export const Area = styled.div`
+margin: auto;
+max-width: 980px;
+padding: 10px;
+border-bottom: 2px;
+`;
+
+export const Header = styled.h1`
+margin: 0;
+padding: 0;
+border-bottom:1px solid #444;
+text-align: center;
+color: #fff;
+padding-bottom: 20px;
+`;
+
